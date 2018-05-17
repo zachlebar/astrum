ba04bce7ff6694914a1422f366270de94f447bd8
Zach LeBar <zachlebar@gmail.com>
2018-05-17 13:01:48 -0400

    Adding ability to fetch components from Figma and auto-generate corresponding Astrum components.

--- manager/astrum-figma.js
+++ manager/astrum-figma.js
@@ -3,7 +3,8 @@ var program = require('commander'),
     fs = require('fs-extra'),
     chalk = require('chalk'),
     inquirer = require('inquirer'),
-    utils = require('./utils');
+    utils = require('./utils'),
+    fetch = require('node-fetch');
 
 utils.init();
 
@@ -86,6 +87,127 @@ function editFigmaSettings() {
 }
 
 /**
+ * Figma API Request helper function
+ */
+function apiRequest(endpoint) {
+  return fetch('https://api.figma.com/v1' + endpoint, {
+      method: 'GET',
+      headers: { "x-figma-token": utils.$data.figma.token }
+  }).then(function(response) {
+      return response.json();
+  }).catch(function (error) {
+      return { err: error };
+  });
+}
+
+/**
+ * Pull the File key from the Figma URL
+ */
+function getFileKey(pageUrl) {
+  // console.log(pageUrl);
+  // pageUrl = pageUrl.replace(/https:\/\/www.figma.com\/file\//, '').replace(/\/.*/,'');
+  // console.log(pageUrl);
+  return pageUrl.replace(/https:\/\/www.figma.com\/file\//, '').replace(/\/.*/,'');
+}
+
+/**
+ * Fetch the Figma components from the existing URL
+ */
+function fetchFigmaComponents() {
+  if (utils.$data.figma) {
+    apiRequest('/files/' + getFileKey(utils.$data.figma.url))
+      .then(function (apiResponse) {
+        console.log();
+        for (nodeID in apiResponse.components) {
+          var component = {};
+
+          if (nodeID.charAt(0) != '-') {
+            component.nodeID = nodeID;
+            component.title = apiResponse.components[nodeID].name;
+            buildComponent(component);
+          }
+        }
+        console.log();
+      }
+    );
+  } else {
+    console.log(chalk.red('No Figma settings found!'));
+    editFigmaSettings();
+  }
+}
+
+/**
+ * Build the Astrum components from the Figma info
+ */
+function buildComponent(component) {
+
+  // make sure we have a valid potential component
+  if (utils.validateComponent(component.title)) {
+    var parts = component.title.split("/");
+    var newComponent = {};
+
+    // fill in our component details with some reasonable defaults
+    newComponent.group = parts[0].toLowerCase();
+    newComponent.name = parts[1].toLowerCase();
+    newComponent.title = parts[1];
+    newComponent.width = 'half';
+    newComponent.nodeID = component.nodeID;
+
+    // if this is a new group
+    if (!utils.groupExists(newComponent.group)) {
+      var newGroup = {};
+
+      newGroup.name = newComponent.group;
+      newGroup.title = parts[0];
+
+      let group_position = utils.$data.groups.length - 1;
+
+      // create the new Group and new Component
+      utils.$data.groups.splice(group_position, 0, newGroup);
+      utils.$data.groups[group_position].components = [newComponent];
+
+      // create the corresponding files
+      if (utils.createComponentFiles(newComponent)) {
+          utils.saveData(function () {
+              console.log();
+              console.log(chalk.grey('----------------------------------------------------------------'));
+              console.log(chalk.green('\u2713 Pattern library data saved successfully.'));
+              console.log(chalk.grey('----------------------------------------------------------------'));
+
+              console.log();
+              console.log(chalk.yellow('Add your group description to ' + utils.$config.path + '/components/' + newComponent.group + '/description.md (Markdown supported)'));
+              console.log(chalk.yellow('Add your component markup to ' + utils.$config.path + '/components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
+              console.log(chalk.yellow('Add your component description to ' + utils.$config.path + '/components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
+              console.log();
+          });
+      }
+    // if this is an existing group
+    } else {
+      var groupIndex = utils.getGroupIndex(newComponent.group);
+      let component_position = utils.getComponentPositionChoices(newComponent.group).length - 1;
+
+      // create the new Component
+      utils.$data.groups[groupIndex].components.splice(component_position, 0, newComponent);
+
+      // create the corresponding files
+      if (utils.createComponentFiles(newComponent)) {
+          utils.saveData(function () {
+              console.log();
+              console.log(chalk.grey('----------------------------------------------------------------'));
+              console.log(chalk.green('\u2713 Pattern library data saved successfully.'));
+              console.log(chalk.grey('----------------------------------------------------------------'));
+
+              console.log();
+              console.log(chalk.yellow('Add your component markup to ' + utils.$config.path + '/components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
+              console.log(chalk.yellow('Add your component description to ' + utils.$config.path + '/components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
+              console.log();
+          });
+      }
+    }
+  }
+}
+
+/**
  * astrum figma
  *
  * prompt to import settings, otherwise displays the help text
@@ -118,4 +240,11 @@ if (process.argv[2] == 'info') {
  */
 if (process.argv[2] == 'edit') {
   editFigmaSettings();
+}
+
+/**
+ * astrum figma fetch
+ */
+if (process.argv[2] == 'fetch') {
+  fetchFigmaComponents();
 }
